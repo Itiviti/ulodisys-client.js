107cbc7cf3b32e6423f58cb81395e67b7afd957b
Jessé LIBEN <jesse.liben@ullink.com>
2018-07-09 13:33:29 +0200

    Fix start method

--- test/index.spec.js
+++ test/index.spec.js
@@ -17,6 +17,15 @@ describe('The odisys client', () => {
     expect(fakeSocket.write.args[1][0].toString()).to.equal('<action type="user.logon" id="1"><user id="foo" password="bar"/></action>')
     expect(emitSpy.withArgs('logged').notCalled)
   })
+  it('should send logon to server when using start helper method', () => {
+    expect(emitSpy.withArgs('connected').calledOnce)
+    // client.start({user: 'foo', password: 'bar', heartbeatInterval: -1})
+    client.start({ host: 'localhost', port: 2098, user: 'foo', password: 'bar', heartbeatInterval: -1 })
+
+    expect(fakeSocket.write.calledThrice)
+    expect(fakeSocket.write.args[1][0].toString()).to.equal('<action type="user.logon" id="1"><user id="foo" password="bar"/></action>')
+    expect(emitSpy.withArgs('logged').notCalled)
+  })
   it('should switch state when ack received', () => {
     fakeSocket.emit('data', new Buffer('<msg><ack actionid="1"/>'))
     expect(emitSpy.withArgs('logged').calledOnce)
